c356ceaeef760e036fdbdb9c2cdcb84dacef9c80
alexkonecny <79099016+alexkonecny@users.noreply.github.com>
2025-01-29 14:35:55 +0100

    Update action.js

--- action.js
+++ action.js
@@ -62,20 +62,13 @@ var links = document.getElementsByClassName("social-media");
 
 links[0].addEventListener("click", () => { openlink(1) });
 links[1].addEventListener("click", () => { openlink(2) });
-links[2].addEventListener("click", () => { openlink(3) });
-links[3].addEventListener("click", () => { openlink(4) });
+
 
 function openlink(x) {
     if (x == 1) {
         window.open("https://www.instagram.com/alexkonecny40/profilecard/?igsh=MWZ3eGNxNTFnNmt1cw==", "_blank");
     }
     if (x == 2) {
-        window.open("https://www.linkedin.com/in/vineet-kumar-gupta-2833ab196/", "_blank");
-    }
-    if (x == 3) {
         window.open("https://github.com/alexkonecny/Rarotonga", "_blank");
-    }
-    if (x == 4) {
-        window.open("https://vineet-portfolio-site.netlify.app/", "_blank");
     }
 }
